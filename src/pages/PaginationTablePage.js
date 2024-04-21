0a651033e6d6f3473d9a077bd813f0a1d245a51f
= <=>
2024-04-21 17:55:48 -0300

    ìnitial commit

---
+++ src/pages/PaginationTablePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import PaginationTable from "../components/PaginationTable";
+
+function PaginationTablePage() {
+  return <PaginationTable />;
+}
+
+export default PaginationTablePage;
